006bc1d022a311b14a82e730d2effa38890068bf
Adeline Charlotte <adeline.chrltt@gmail.com>
2026-02-03 20:29:02 +0700

    Squash merge feat/initial-setup into dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
+import './ColorTokens.css'
+
+import LandingPage from './pages/LandingPage'
+import Layout from './BaseLayout'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<LandingPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
