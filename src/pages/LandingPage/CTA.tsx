ebdefbbf0fae83b72015b320d64af2e9fbeaba9f
Adeline Charlotte <adeline.chrltt@gmail.com>
2026-02-04 17:58:46 +0700

    feat: Update CTA section
    - Update typography and spacing

--- src/pages/LandingPage/CTA.tsx
+++ src/pages/LandingPage/CTA.tsx
@@ -2,15 +2,40 @@ import SmallButton, { SmallBtnType } from "../../components/Button"
 
 export default function CTA() {
     return (
-        <>
-            <h1 className="rufina-bold burgundy">
-                Let's work together.
+        <section style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            minHeight: "100vh",
+            gap: `${30 / 16}rem`
+        }}>
+            <h1 className="rufina-bold burgundy" style={{
+                fontSize: `${128 / 16}rem`,
+                lineHeight: "0.7",
+                margin: "0rem",
+                marginBottom: "1.5rem"
+            }}>
+                Let's work <span style={{
+                    textDecorationLine: "underline",
+                    textDecorationThickness: "0.4rem",
+                    textUnderlineOffset: "0.7rem"
+                }}>together.</span>
             </h1>
-            <div>
-                <p className="instrument-sans">
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: `${10 / 16}rem`,
+                width: "100%"
+            }}>
+                <p className="instrument-sans" style={{ fontSize: `${24 / 16}rem`, margin: 0 }}>
                     Get in touch with me!
                 </p>
-                <span>
+                <span style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: `${10 / 16}rem`
+                }}>
                     <SmallButton
                         onClick={() => { console.log("pressed") }}
                         text={"Hooray"}
@@ -21,22 +46,30 @@ export default function CTA() {
                         type={SmallBtnType.red} />
                     <SmallButton
                         onClick={() => { console.log("pressed") }}
-                        text={"Hooray"}
-                        type={SmallBtnType.red} />
+                        text={"CV"}
+                        type={SmallBtnType.red}
+                        icon={"src/assets/icons/CV.svg"} />
                 </span>
             </div>
 
-            <div>
-                <p className="instrument-sans">
-                    Or contact me via my socials.
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "end",
+                gap: `${10 / 16}rem`,
+                width: "100%"
+            }}>
+                <p className="instrument-sans" style={{ fontSize: `${24 / 16}rem`, margin: 0 }}>
+                    Or contact me via <span className="blue-1"><b>my socials</b></span>.
                 </p>
                 <span>
                     <SmallButton
                         onClick={() => { console.log("pressed") }}
-                        text={"Hooray"}
-                        type={SmallBtnType.blue} />
+                        text={"About me"}
+                        type={SmallBtnType.blue}
+                        icon={"src/assets/Arrow.svg"} />
                 </span>
             </div>
-        </>
+        </section >
     )
 }
