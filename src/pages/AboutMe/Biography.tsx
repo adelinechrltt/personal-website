fd99157d372c18c1e7d87d0db1e87392a64138a9
Adeline Charlotte <adeline.chrltt@gmail.com>
2026-02-05 17:11:07 +0700

    feat: Adjust Biography section typography

--- src/pages/AboutMe/Biography.tsx
+++ src/pages/AboutMe/Biography.tsx
@@ -2,29 +2,71 @@ export default function Biography() {
     return (
         <section>
             <div>
-                <h1>Adeline</h1>
-                <h2>Aspiring software developer & lifelong learner</h2>
+
+                <h1 className="rufina-bold blue-1 shadow-text"
+                    style={{
+                        fontSize: `${200 / 1512 * 100}vw`,
+                        margin: 0,
+                        marginBottom: "-1.5rem"
+                    }}>
+                    Adeline.
+                </h1>
+
+                <h2 className="reenie-beanie-regular text-dark-gray"
+                    style={{
+                        fontSize: `${40 / 1512 * 100}vw`,
+                        WebkitTextStroke: "1px",
+                        margin: "0rem",
+                    }}>
+                    Aspiring software developer & lifelong learner
+                </h2>
+
             </div>
+
             <div>
                 <div>
-                    <h3>Hello!</h3>
-                    <p>
-                        I’m <b>Adeline</b>, an aspiring full-stack software developer based in{" "}
-                        <b>Jakarta, Indonesia.</b>
-                    </p>
+                    <h3 className="rufina-bold burgundy"
+                        style={{
+                            fontSize: `${32 / 1512 * 100}vw`,
+                        }}>
+                        Hello!
+                    </h3>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: `10px`
+                    }}>
+                        <p className="instrument-sans" style={{
+                            fontSize: `${18 / 1512 * 100}vw`,
+                            margin: 0,
+                        }}>
+                            I’m <b>Adeline</b>, an aspiring full-stack software developer based in{" "}
+                            <b>Jakarta, Indonesia.</b>
+                        </p>
 
-                    <ul>
-                        <li>
-                            A lifelong learner with a deep curiosity for how things work, passion for
-                            turning ideas into real-world solutions, and dedication to growth.
-                        </li>
-                        <li>
-                            Currently exploring iOS and backend development, system design, and software
-                            architecture!
-                        </li>
-                    </ul>
+                        <ul className="instrument-sans"
+                            style={{
+                                fontSize: `${18 / 1512 * 100}vw`,
+                                margin: 0,
+                                display: "flex",
+                                flexDirection: "column",
+                                gap: "10px"
+                            }}>
+                            <li>
+                                A <b>lifelong learner</b> with a deep curiosity for how things work, passion for
+                                turning ideas into real-world solutions, and dedication to growth.
+                            </li>
+                            <li>
+                                Currently exploring <b>
+                                    iOS and backend development, system design, and software
+                                    architecture!
+                                </b>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
+
             <div style={{
                 display: "flex",
                 flexDirection: "row",
@@ -32,26 +74,36 @@ export default function Biography() {
                 <div style={{
                     width: "50%"
                 }}>
-                    <h3>Socials</h3>
-                    <span>
+                    <h3 className="rufina-bold blue-2"
+                        style={{
+                            fontSize: `${32 / 1512 * 100}vw`,
+                            margin: 0
+                        }}>
+                        Socials
+                    </h3>
+                    <span style={{ margin: 0 }}>
                         <img src="src/assets/icons/LinkedIn.svg" alt="" style={{ width: `${30 / 1512 * 100}vw` }} />
                         <img src="src/assets/icons/Instagram.svg" alt="" style={{ width: `${30 / 1512 * 100}vw` }} />
                         <img src="src/assets/icons/GitHub.svg" alt="" style={{ width: `${30 / 1512 * 100}vw` }} />
                         <img src="src/assets/icons/Spotify.svg" alt="" style={{ width: `${30 / 1512 * 100}vw` }} />
                     </span>
                     <div>
-                        <p>Last listened to...</p>
+                        <p className="instrument-sans-medium" style={{ fontSize: `${16 / 1512 * 100}vw` }}><i>Last listened to...</i></p>
                         <div style={{
                             display: "flex",
                             flexDirection: "row"
                         }}>
                             <img src="src/assets/icons/Spotify.svg" alt="" style={{ width: `${75 / 1512 * 100}vw` }} />
-                            <div style={{
+                            <div className="instrument-sans" style={{
                                 display: "flex",
                                 flexDirection: "column",
                                 justifyItems: "flex-start"
                             }}>
-                                <span><img src="src/assets/icons/MusicIcon.svg" alt="" /><p>Jane Doe</p></span>
+                                <span style={{
+                                    display: "flex",
+                                    flexDirection: "row",
+                                    justifyItems: "flex-start"
+                                }}><img src="src/assets/icons/MusicIcon.svg" alt="" /><p><b>Jane Doe</b></p></span>
                                 <p>by Kenshi Yonezu & Hikaru Utada</p>
                                 <p>2d ago via Spotify Web API</p>
                             </div>
@@ -61,28 +113,34 @@ export default function Biography() {
                 <div style={{
                     width: "50%"
                 }}>
-                    <h3>Trivia</h3>
-                    <ul>
-                        <li>I'm an <b>INTJ-T</b> and a <b>huge cat person!</b></li>
+                    <h3 className="rufina-bold blue-2"
+                        style={{
+                            fontSize: `${32 / 1512 * 100}vw`,
+                            margin: 0
+                        }}>
+                        Trivia
+                    </h3>
+                    <ul className="instrument-sans" style={{ fontSize: `${15 / 1512 * 100}vw` }}>
+                        <li><b>INTJ-T</b> and a <b>huge cat person~</b></li>
 
                         <li>
-                            My current, top two interests span:
+                            Several of the things I like include:
                             <ul>
                                 <li>
-                                    <b>the Godzilla franchise:</b> I &lt;3 the Monsterverse and Shin Godzilla entries
+                                    <b>the Godzilla franchise,</b>  especially Monsterverse & Shin Godzilla
                                 </li>
                                 <li>
-                                    <b>Nintendo games:</b> huge fan of The Legend of Zelda & Fire Emblem series!
+                                    <b>Nintendo games</b> (The Legend of Zelda & Fire Emblem)
                                 </li>
                             </ul>
                         </li>
 
                         <li>
-                            Avid listener of <b>East Asian indie & urban-pop</b>
-                            (Hikaru Utada, Ken Hirai, m-flo, Elephant Gym, Sunset Rollercoaster)
+                            <b>East Asian indie & urban-pop</b>
+                            (Hikaru Utada, m-flo, Elephant Gym)
                         </li>
 
-                        <li>I also do <b><a>digital art</a></b> on the side too, sometimes!</li>
+                        <li>I also do <a className="blue-2"><b><u>digital art</u></b></a> on the side too, sometimes!</li>
                     </ul>
                 </div>
             </div>
